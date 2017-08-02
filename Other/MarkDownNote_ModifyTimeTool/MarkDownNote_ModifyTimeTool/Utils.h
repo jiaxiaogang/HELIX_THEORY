454beee0344fed1ddbb0348bba4cca9ee8584e73
贾晓刚 <jxg2764894@live.com>
2017-08-02 20:13:04 +0800

    修改时间工具;

---
+++ Other/MarkDownNote_ModifyTimeTool/MarkDownNote_ModifyTimeTool/Utils.h
@@ -0,0 +1,15 @@
+//
+//  Utils.h
+//  MarkDownNote_ModifyTimeTool
+//
+//  Created by 贾  on 2017/8/2.
+//  Copyright © 2017年 XiaoGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utils : NSObject
+
++ (NSString *)getAppPath;
+
+@end
